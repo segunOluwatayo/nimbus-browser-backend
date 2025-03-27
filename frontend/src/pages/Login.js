d2cce18d55e2dacd6476f1e3bceb9a64b9956f23
“Segun <Oluwatayoo08@gmail.com>
2025-03-27 14:56:39 +0000

    Refactor Login component imports; remove unused useLocation import

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { useLocation, useSearchParams } from 'react-router-dom';
+import {useSearchParams } from 'react-router-dom';
 import { 
   Container, 
   Typography, 
